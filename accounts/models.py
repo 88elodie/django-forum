e78916309beeedc527f6cf21650bcfc4b8e7b7fd
Élodie <83143421+88elodie@users.noreply.github.com>
2023-06-22 16:00:43 -0400

    fix sign up
    sign up did not work because of wrong form reference in view

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
+    email = models.EmailField(("email address"), unique=True)
 
     def __str__(self):
         return self.username
+    
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from accounts.models import CustomUser
+from django.conf import settings
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
