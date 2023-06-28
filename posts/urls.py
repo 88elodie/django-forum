ed577b8f840c41f88f5158f19d76f4444382b7e1
Élodie <83143421+88elodie@users.noreply.github.com>
2023-06-28 15:01:43 -0400

    edit post

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='posts'),
     path('create/', views.CreatePost, name='create-posts'),
     path('<int:pk>/', views.SinglePost, name='single-post'),
+    path('edit/<int:pk>/', views.EditPost, name='edit-post'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404, reverse
 from django.http import HttpResponse
 from .forms import PostForm
 from django.urls import reverse_lazy
@@ -15,7 +15,7 @@ def index(request):
     return render(request, 'posts_index.html', context={"posts": posts})
 
 def SinglePost(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, id=pk)
     return render(request, 'single_post.html',context={'post': post})
 
 @login_required
@@ -33,4 +33,23 @@ def CreatePost(request):
             messages.error(request, 'Please correct the error below.')
     else:
         post_form = PostForm()
-    return render(request, "create_post.html", context={"form": post_form})
+    return render(request, "post_form.html", context={"form": post_form})
+
+def EditPost(request, pk):
+    post = get_object_or_404(Post, id=pk)
+
+    if request.method == 'GET':
+        context = {'form': PostForm(instance=post), 'id': pk}
+        return render(request, 'post_form.html', context)
+    elif request.method == 'POST':
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+            messages.success(request, 'The post has been updated successfully.')
+            post_id = post.id
+            url = reverse('single-post', args=[post_id])
+            return redirect(url)
+        else:
+            messages.error(request, 'Please correct the following errors')
+    return render(request, "post_form.html", context={"form": post_form})
+
