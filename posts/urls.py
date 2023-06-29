dac09df1a081d2fd63c197ae5cda5a4151072f1f
Élodie <83143421+88elodie@users.noreply.github.com>
2023-06-29 14:17:09 -0400

    create post button

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='posts'),
-    path('create/', views.CreatePost, name='create-posts'),
+    path('create/', views.CreatePost, name='create-post'),
     path('<int:pk>/', views.SinglePost, name='single-post'),
     path('edit/<int:pk>/', views.EditPost, name='edit-post'),
     path('delete/<int:pk>/', views.DeletePost, name='delete-post'),
