f69227132ccfccd92630d40e0b1676d1c2cd8d54
Élodie <83143421+88elodie@users.noreply.github.com>
2023-07-12 12:20:41 -0400

    minor app.js adjustment

--- static/app.js
+++ static/app.js
@@ -16,39 +16,27 @@ const csrfToken = getCookie('csrftoken');
 FilePond.setOptions({
     server: {
         url: 'http://localhost:8000/fp',
+        headers: {
+            'X-CSRFToken': csrfToken,
+          },
         process: {
             url: '/process/',
-            headers: {
-              'X-CSRFToken': csrfToken,
-            },
             withCredentials: true,
           },
         patch: {
             url: '/patch/',
-            headers: {
-              'X-CSRFToken': csrfToken,
-            },
             withCredentials: true,
           },
         revert: {
             url: '/revert/',
-            headers: {
-              'X-CSRFToken': csrfToken,
-            },
             withCredentials: true,
           },
         fetch: {
             url: '/fetch/?target=',
-            headers: {
-              'X-CSRFToken': csrfToken,
-            },
             withCredentials: true,
           },
         load: {
             url: '/load/',
-            headers: {
-              'X-CSRFToken': csrfToken,
-            },
             withCredentials: true,
           },
     }
@@ -60,5 +48,5 @@ const pond = FilePond.create(inputElement, {
     maxFiles: 3,
     maxFileSize: '2MB',
     acceptedFileTypes: ['image/*'],
-    files: imageUrls.length > 0 ? imageUrls.map(url => ({ source: '?id='+url.fields.upload_id, options: { type: 'local' } })) : []
+    files:[{ source: '?id='+imageUrls[0].fields.upload_id, options: { type: 'local' }}]
 });
