9cc75c761eb79a0b477ce3e733792358e821a094
Élodie <83143421+88elodie@users.noreply.github.com>
2023-07-19 15:18:35 -0400

    maintain gif images on upload

--- posts/models.py
+++ posts/models.py
@@ -4,21 +4,27 @@ from django.conf import settings
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.utils import timezone
 
+
 # Create your models here.
 
+class Board(models.Model):
+    name = models.CharField
+    
+
 class Post(models.Model):
-    title = models.CharField(max_length=50, validators = [
-            MinLengthValidator(1, 'your title needs at least 1 character')
-        ])
+    title = models.CharField(max_length=50, validators=[
+        MinLengthValidator(1, 'your title needs at least 1 character')
+    ])
     body = models.TextField(max_length=5000,
-        validators = [
-            MinLengthValidator(5, 'your post needs at least 5 characters'),
-            MaxLengthValidator(5000, 'your post can only have 5000 characters')
-        ]
-    )
+                            validators=[
+                                MinLengthValidator(5, 'your post needs at least 5 characters'),
+                                MaxLengthValidator(5000, 'your post can only have 5000 characters')
+                            ]
+                            )
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
 class File(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -28,12 +34,13 @@ class File(models.Model):
 
     def __str__(self):
         return self.file
-    
+
+
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    comment = models.TextField(max_length=1000, validators = [
-            MinLengthValidator(1, 'your comment needs at least 1 character'),
-            MaxLengthValidator(1000, 'your comment can only have 300 characters')
-        ])
+    comment = models.TextField(max_length=1000, validators=[
+        MinLengthValidator(1, 'your comment needs at least 1 character'),
+        MaxLengthValidator(1000, 'your comment can only have 300 characters')
+    ])
     created_at = models.DateTimeField(auto_now_add=True)
