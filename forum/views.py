717fc1f9d5d325f15830679be4cdf6bb8671a305
Élodie <83143421+88elodie@users.noreply.github.com>
2023-07-19 20:21:51 -0400

    board style pt1

--- forum/views.py
+++ forum/views.py
@@ -1,8 +1,11 @@
-from posts.models import Board
+from posts.models import Board, Post
 from django.shortcuts import render
 
 def home(request):
     boards = Board.objects.all()
+    for board in boards:
+        post_count = Post.objects.filter(board_id=board.id).count()
+        board.post_count = post_count
 
     return render(request, 'home.html', context={"boards":boards})
 
