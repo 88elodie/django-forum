082912d4a1a0beeb3ffd3e2a73c10bd80888b3af
Élodie <83143421+88elodie@users.noreply.github.com>
2023-07-23 11:06:49 -0400

    alert count context processor

---
+++ forum/context_processors.py
@@ -0,0 +1,9 @@
+from accounts.models import Alert
+
+def alerts_count(request):
+    if request.user.is_authenticated:
+        alerts_count = Alert.objects.filter(user=request.user, read=False).count()
+    else:
+        alerts_count = 0
+    
+    return {'alerts_count': alerts_count}
