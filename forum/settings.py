fb7921abc91b262ea000307571c76c2d86996bc7
Élodie <83143421+88elodie@users.noreply.github.com>
2023-08-07 11:34:17 -0400

    email env variables

--- forum/settings.py
+++ forum/settings.py
@@ -164,10 +164,10 @@ LOGOUT_REDIRECT_URL = "home"
 # Email server
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mailer.ep88@gmail.com'
-EMAIL_HOST_PASSWORD = 'qbxvutryeeutpmja'
+EMAIL_HOST = os.environ["EMAIL_HOST"]
+EMAIL_PORT = os.environ["EMAIL_PORT"]
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 
 # Custom user model
 
